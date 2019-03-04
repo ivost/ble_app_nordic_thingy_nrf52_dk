4570d6263f0fc9ca704e4c9e96a0b142a31c41ce
Nebiyou Yismaw <nyismaw@o-nyismaw.cardinalpeak.com>
2019-03-04 10:19:12 -0700

    First Commit

---
+++ pca10040/s132/ses/Output/ble_app_blinky_c_pca10040_s132 Release/Obj/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/ble_app_blinky_c_pca10040_s132\ Release/Obj/ses_startup_nrf_common.o: \
+ /Users/nyismaw/Downloads/nRF5_SDK_15.2.0_9412b96/modules/nrfx/mdk/ses_startup_nrf_common.s
---
+++ pca10040/s132/ses/Output/ble_app_blinky_c_pca10040_s132 Release/Obj/utf.d
@@ -0,0 +1,6 @@
+Output/ble_app_blinky_c_pca10040_s132\ Release/Obj/utf.o: \
+ /Users/nyismaw/Downloads/nRF5_SDK_15.2.0_9412b96/external/utf_converter/utf.c \
+ /Users/nyismaw/Downloads/nRF5_SDK_15.2.0_9412b96/external/utf_converter/utf.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stddef.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/__crossworks.h \
+ /Applications/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.12/include/stdint.h
